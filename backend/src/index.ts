4d181a3b10ce000a2227ee1c4cc01ea0a0c96e2d
Tommaso Bruno <bruno.tommaso31@gmail.com>
2020-11-19 12:48:34 +0000

    server basic middlewares and listening funciton implemtented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log('ts is working 2');
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+
+require('dotenv').config();
+
+const app: express.Application = express();
+
+app.use(helmet());
+app.use(cors({
+    origin: '*'
+}));
+
+const port: string = process.env.SERVER_PORT || '5000';
+
+
+app.listen(port, () => {
+
+    console.log(`Server listening on port ${port}`);
+});
