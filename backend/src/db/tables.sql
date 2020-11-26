6d426ca310d395564d2b5aa2c84c2c09d7e076d9
Tommaso Bruno <bruno.tommaso31@gmail.com>
2020-11-26 14:02:29 +0000

    tables.sql fixed

--- backend/src/db/tables.sql
+++ backend/src/db/tables.sql
@@ -1,35 +1,40 @@
 CREATE TABLE user (
-    id: varchar(40) not null primary key,
-    first_name: varchar(30) not null,
-    last_name: varchar(30) not null,
-    email: varchar(50) not null,
-    is_admin: boolean not null,
-    last_login: date not null
-)
+    id varchar(40) not null,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    email varchar(50) not null,
+    is_admin boolean not null,
+    last_login date not null,
+    primary key(id)
+);
 
 CREATE TABLE team (
-    id: varchar(40) not null primary key,
-    description: varchar(50),
-)
+    id varchar(40) not null,
+    description varchar(50),
+    primary key(id)
+);
 
 CREATE TABLE user_team (
-    id: varchar(40) not null primary key,
-    user_id: varchar(40) not null,
-    team_id: varchar(40) not null,
+    id varchar(40) not null,
+    user_id varchar(40) not null,
+    team_id varchar(40) not null,
+    primary key(id),
     foreign key (user_id) references user(id),
     foreign key (team_id) references team(id)
-)
+);
 
 CREATE TABLE project (
-    id: varchar(40) not null primary key,
-    name: varchar(30),
-    priority: integer
-)
+    id varchar(40) not null,
+    name varchar(30),
+    priority int,
+    primary key(id)
+);
 
 CREATE TABLE user_project (
-    id: varchar(40) not null primary key,
-    user_id: varchar(40) not null,
-    project_id: varchar(40) not null,
+    id varchar(40) not null,
+    user_id varchar(40) not null,
+    project_id varchar(40) not null,
+    primary key(id),
     foreign key (user_id) references user(id),
     foreign key (project_id) references project(id)
-)
+);
