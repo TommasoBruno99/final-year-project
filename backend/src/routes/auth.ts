437d3d1c234bf2f33ad251b81203d2fa6c0a8357
Tommaso Bruno <bruno.tommaso31@gmail.com>
2020-11-26 15:51:20 +0000

    constants typescript file implemented in services

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import * as ROUTEs from '../constants/routes';
 import AuthService from '../services/auth';
 
 const authRouter: Router = Router();
 
-authRouter.get('/', AuthService.loginService);
+authRouter.get(ROUTES.LOGIN, AuthService.loginService);
 
 
 export default authRouter;
