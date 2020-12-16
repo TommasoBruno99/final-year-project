d13599f240ecd3ce49f0a7ea38bae263991de4de
Tommaso Bruno <bruno.tommaso31@gmail.com>
2020-12-16 04:15:24 +0000

    Added api version for API ROUTES

--- backend/src/constants/routes.ts
+++ backend/src/constants/routes.ts
@@ -1,2 +1,5 @@
+export const API_VERSION = '/api/v1';
+
+
 export const LOGIN = '/login';
 export const REGISTER = '/register';
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import * as errorMiddleware from '../middlewares/errorHandling';
+import { API_VERSION } from '../constants/routes';
 import authRouter from './auth';
 
 const mainRouter: Router = Router();
 
-mainRouter.use('/auth', authRouter);
+mainRouter.use(API_VERSION + '/auth', authRouter);
 
 
 mainRouter.use(errorMiddleware.notFound);
