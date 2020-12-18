2353e64db2bdd884c1dddccf5aa2d4f53468d8f1
Tommaso <bruno.tommaso31@gmail.com>
2020-12-18 15:51:05 +0000

    database connection fixed

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,5 +1,13 @@
 import mysqlDB from 'mysql';
 
 const connection: mysqlDB.Connection = mysqlDB.createConnection({
-    host: ''
+    host: 'localhost',
+    database: 'scheduling_project',
+    user: 'root',
+    password: 'password',
+    port: 3306
 });
+
+connection.connect();
+
+export default connection;
