6a20c7475d0cd8f23e22456a81d6558ee473c8ff
Tommaso <bruno.tommaso31@gmail.com>
2021-01-24 16:47:11 +0000

    removed comments in code

--- backend/src/services/auth.ts
+++ backend/src/services/auth.ts
@@ -17,22 +17,19 @@ class AuthService implements IAuthService {
             const resultQuery: RegisterUser[] = await AuthRepository.findUserEmail(newUser.email);
             
             if (resultQuery.length > 0) {
-                // User already exists
+                
                 throw new Error('Email already in use');
             } else { 
 
-                // Generate unique id 
                 const uniqueId: string = await v4();
-
-                // Generate hash of password
                 const hash = await bcrypt.hash(newUser.password, 8);
-
                 await AuthRepository.insertNewUser(uniqueId, newUser.first_name, newUser.last_name, newUser.email, hash, false, new Date());
-
+                
                 return [true, {}];
             }
 
         } catch(error) {
+
             return [false, error.message];
         }
     }
@@ -45,7 +42,6 @@ class AuthService implements IAuthService {
 
             if (resultQuery.length > 0) {
 
-                // User found, check password
                 if (await bcrypt.compare(loginUser.password, resultQuery[0].password)) {
 
                     const returnObj = {
@@ -63,13 +59,16 @@ class AuthService implements IAuthService {
                     
                     return [true, jsonwebtoken];
                 } else {
+
                     throw new Error('Problem during login');
                 }
             } else {
+
                 throw new Error('Problem during login');
             }
             
         } catch(error) {
+            
             return [false, error.message];
         }
     }
