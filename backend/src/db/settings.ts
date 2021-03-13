08cd688e54e46a98adaaa7c0bcf77601a2ea98a9
Tommaso <bruno.tommaso31@gmail.com>
2021-03-13 19:14:50 +0000

    settings database updated

--- backend/src/db/settings.ts
+++ backend/src/db/settings.ts
@@ -1,10 +1,9 @@
 import { ConnectionConfig } from "mysql";
 
 export const database_config: ConnectionConfig = {
-  host: process.env.HOST,
-  database: process.env.DATABASE_NAME,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  port: parseInt(process.env.DATABASE_PORT!),
+  host: "localhost",
+  database: "scheduling_project",
+  user: "root",
+  password: "password",
+  port: 3306,
 };
