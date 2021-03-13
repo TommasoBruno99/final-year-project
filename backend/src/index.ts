bcc8423d32c216704ee95879a2fcbe0f67cc42bb
Tommaso <bruno.tommaso31@gmail.com>
2021-03-13 19:05:40 +0000

    disable eslint for dotenv require

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import mainRouter from './routes';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import mainRouter from "./routes";
 
-
-require('dotenv').config();
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("dotenv").config();
 
 const app: express.Application = express();
 
 app.use(helmet());
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(mainRouter);
 
-const port: string = process.env.SERVER_PORT || '5000';
-
+const port: string = process.env.SERVER_PORT || "5000";
 
 app.listen(port, (): void => {
-
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
